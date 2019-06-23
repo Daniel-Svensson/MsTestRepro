5c52d3f9dbcdb4c2b66cdad13037ae93931da7f4
Daniel Svensson <daniel.svensson@hotmail.se>
2019-06-23 14:35:50 +0200

    simplify repro to almost minimum

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -18,88 +18,14 @@ namespace ClassLibrary1
         public static (Assembly, List<Type>) FindTypes(string assemblyPath, string name)
         {
             // TODO: use mef to load assembly and look for type
-            var assembly = LoadAssembly(assemblyPath);
-          
+            var assemblyName = AssemblyName.GetAssemblyName(assemblyPath);
+            var assembly = Assembly.Load(assemblyName);
+     
             var classes = assembly.GetExportedTypes()
-                .Where(t => t.Name.StartsWith(name))
+                .Where(t => t.Name == name)
                 .ToList();
             return (assembly, classes);
         }
 
-        internal static Assembly LoadAssembly(string assemblyFileName)
-        {
-            System.Diagnostics.Debug.Assert(!string.IsNullOrEmpty(assemblyFileName), "assemblyFileName is required");
-
-            Assembly assembly = null;
-
-            // TODO: [roncain] Dev10 temp workaround.
-            // Reference assemblies will fail by file path, so try to load via name
-            // and silently accept that result.  If fail, let normal file load try.
-            try
-            {
-                AssemblyName asmName = AssemblyName.GetAssemblyName(assemblyFileName);
-                assembly = LoadAssembly(asmName);
-                if (assembly != null)
-                {
-                    return assembly;
-                }
-
-                // Otherwise attempt to load from file
-                assembly = Assembly.LoadFrom(assemblyFileName);
-            }
-            catch (Exception ex)
-            {
-                // Some common exceptions log a warning and keep running
-                if (ex is System.IO.FileNotFoundException ||
-                    ex is System.IO.FileLoadException ||
-                    ex is System.IO.PathTooLongException ||
-                    ex is BadImageFormatException ||
-                    ex is System.Security.SecurityException)
-                {
-
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return assembly;
-        }
-
-        /// <summary>
-        /// Loads the specified assembly by name.
-        /// </summary>
-        /// <param name="assemblyName">The name of the assembly to load.  It cannot be null.</param>
-        /// 
-        /// <returns>The loaded <see cref="Assembly"/> if successful, null if it could not be loaded for a known reason
-        /// (and an error message will have been logged).
-        /// </returns>
-        internal static Assembly LoadAssembly(AssemblyName assemblyName)
-        {
-            System.Diagnostics.Debug.Assert(assemblyName != null, "assemblyName is required");
-
-            Assembly assembly = null;
-
-            try
-            {
-                assembly = Assembly.Load(assemblyName);
-            }
-            catch (Exception ex)
-            {
-                // Some common exceptions log a warning and keep running
-                if (ex is System.IO.FileNotFoundException ||
-                    ex is System.IO.FileLoadException ||
-                    ex is System.IO.PathTooLongException ||
-                    ex is BadImageFormatException ||
-                    ex is System.Security.SecurityException)
-                {
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return assembly;
-        }
     }
 }
